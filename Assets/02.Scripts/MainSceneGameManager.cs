3f3efdaeb46a55687ba976e3cc3c9cae4c64a4c0
LHT <hyuntak999@gmail.com>
2025-01-31 17:14:03 +0900

    각 버튼마다 기능 구현화 중
    스테이지 버튼에 맞는 스테이지 이동하게 구현중

--- Assets/02.Scripts/MainSceneGameManager.cs
+++ Assets/02.Scripts/MainSceneGameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainSceneGameManager : MonoBehaviour
 {
@@ -19,6 +22,11 @@ public class MainSceneGameManager : MonoBehaviour
 
     private float fadeTime = 0; // 처음 알파값
 
+    private bool isStageScene = false; // 스테이지 선택 씬 활성화 여부
+
+    [SerializeField]
+    private Animator[] selectButton; // 스테이지 버튼 애니메이션
+
     // Start 버튼을 눌렀을 때
     public void StartButton()
     {
@@ -26,6 +34,7 @@ public class MainSceneGameManager : MonoBehaviour
         StartCoroutine(FadeCoroutine()); // 코루틴 실행
     }
 
+    // 화면이 어두워지는 코루틴 실행
     IEnumerator FadeCoroutine()
     {
         while (fadeTime < 1.0f) // 알파값이 1이 될 때까지 반복
@@ -36,7 +45,16 @@ public class MainSceneGameManager : MonoBehaviour
         }
         if (fadeTime > 0.9f) // 배경이 완전히 어두워 졌을 때
         {
-            levelSelect.SetActive(true); // 스테이지 선택 UI 활성화
+            if(!isStageScene) // LevelSelect 창이 활성화 되지 않을 경우
+            {
+                levelSelect.SetActive(true); // 스테이지 선택 UI 활성화
+                isStageScene = true;
+            }
+            else
+            {
+                levelSelect.SetActive(false); //  LevelSelect 창이 활성화 됬을 경우
+                isStageScene = false;
+            }
             StartCoroutine(FadeInCoroutine()); // 화면이 밝아지는 코루틴 실행
         }
     }
@@ -84,5 +102,371 @@ public class MainSceneGameManager : MonoBehaviour
     public void ExitMouseExit()
     {
         exitAnim.SetBool("IsMouse", false);
+    }
+
+    // Back버튼을 눌렀을 때
+    public void Back()
+    {
+        panel.SetActive(true); // 패널 활성화
+        StartCoroutine(FadeCoroutine());
+    }
+
+    // 스테이지 버튼을 눌렀을 때
+    public void Stage1()
+    {
+        StartCoroutine(LoadCoroutine()); // 코루틴 실행
+        panel.SetActive(true); // 패널 활성화
+    }
+
+    public void Stage2()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+
+    public void Stage3()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+
+    public void Stage4()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+
+    public void Stage5()
+    {
+        SceneManager.LoadScene("Stage5");
+    }
+
+    public void Stage6()
+    {
+        SceneManager.LoadScene("Stage6");
+    }
+
+    public void Stage7()
+    {
+        SceneManager.LoadScene("Stage7");
+    }
+
+    public void Stage8()
+    {
+        SceneManager.LoadScene("Stage8");
+    }
+
+    public void Stage9()
+    {
+        SceneManager.LoadScene("Stage9");
+    }
+
+    public void Stage10()
+    {
+        SceneManager.LoadScene("Stage10");
+    }
+
+    public void Stage11()
+    {
+        SceneManager.LoadScene("Stage11");
+    }
+
+    public void Stage12()
+    {
+        SceneManager.LoadScene("Stage12");
+    }
+
+    public void Stage13()
+    {
+        SceneManager.LoadScene("Stage13");
+    }
+
+    public void Stage14()
+    {
+        SceneManager.LoadScene("Stage14");
+    }
+
+    public void Stage15()
+    {
+        SceneManager.LoadScene("Stage15");
+    }
+
+    public void Stage16()
+    {
+        SceneManager.LoadScene("Stage16");
+    }
+
+    public void Stage17()
+    {
+        SceneManager.LoadScene("Stage17");
+    }
+
+    public void Stage18()
+    {
+        SceneManager.LoadScene("Stage18");
+    }
+
+    public void Stage19()
+    {
+        SceneManager.LoadScene("Stage19");
+    }
+
+    public void Stage20()
+    {
+        SceneManager.LoadScene("Stage20");
+    }
+
+    public void Stage21()
+    {
+        SceneManager.LoadScene("Stage21");
+    }
+
+    public void Stage22()
+    {
+        SceneManager.LoadScene("Stage22");
+    }
+
+    // 마우스를 버튼 위에 가져다 댔을 경우
+    public void Stage1ButtonEnter()
+    {
+        selectButton[0].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage2ButtonEnter()
+    {
+        selectButton[1].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage3ButtonEnter()
+    {
+        selectButton[2].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage4ButtonEnter()
+    {
+        selectButton[3].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage5ButtonEnter()
+    {
+        selectButton[4].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage6ButtonEnter()
+    {
+        selectButton[5].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage7ButtonEnter()
+    {
+        selectButton[6].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage8ButtonEnter()
+    {
+        selectButton[7].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage9ButtonEnter()
+    {
+        selectButton[8].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage10ButtonEnter()
+    {
+        selectButton[9].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage11ButtonEnter()
+    {
+        selectButton[10].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage12ButtonEnter()
+    {
+        selectButton[11].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage13ButtonEnter()
+    {
+        selectButton[12].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage14ButtonEnter()
+    {
+        selectButton[13].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage15ButtonEnter()
+    {
+        selectButton[14].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage16ButtonEnter()
+    {
+        selectButton[15].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage17ButtonEnter()
+    {
+        selectButton[16].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage18ButtonEnter()
+    {
+        selectButton[17].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage19ButtonEnter()
+    {
+        selectButton[18].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage20ButtonEnter()
+    {
+        selectButton[19].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage21ButtonEnter()
+    {
+        selectButton[20].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage22ButtonEnter()
+    {
+        selectButton[21].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void BackEnter()
+    {
+        selectButton[22].SetBool("IsMouse", true); // 애니메이션 실행
+    }
+
+    public void Stage1Exit()
+    {
+        selectButton[0].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage2Exit()
+    {
+        selectButton[1].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage3Exit()
+    {
+        selectButton[2].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage4Exit()
+    {
+        selectButton[3].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage5Exit()
+    {
+        selectButton[4].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage6Exit()
+    {
+        selectButton[5].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage7Exit()
+    {
+        selectButton[6].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage8Exit()
+    {
+        selectButton[7].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage9Exit()
+    {
+        selectButton[8].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage10Exit()
+    {
+        selectButton[9].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage11Exit()
+    {
+        selectButton[10].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage12Exit()
+    {
+        selectButton[11].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage13Exit()
+    {
+        selectButton[12].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage14Exit()
+    {
+        selectButton[13].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage15Exit()
+    {
+        selectButton[14].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage16Exit()
+    {
+        selectButton[15].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage17Exit()
+    {
+        selectButton[16].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage18Exit()
+    {
+        selectButton[17].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage19Exit()
+    {
+        selectButton[18].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage20Exit()
+    {
+        selectButton[19].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage21Exit()
+    {
+        selectButton[20].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void Stage22Exit()
+    {
+        selectButton[21].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    public void BackExit()
+    {
+        selectButton[22].SetBool("IsMouse", false); // 애니메이션 실행
+    }
+
+    IEnumerator LoadCoroutine()
+    {
+        float loadTime = 0; // 로딩 시간
+        while (loadTime < 3.0f) // 알파값이 1이 될 때까지 반복
+        {
+            loadTime += 0.01f;
+            yield return new WaitForSeconds(0.01f); // 0.01초마다 실행
+            panelImage.color = new Color(0, 0, 0, loadTime); // 해당 패널의 투명도 조절
+        }
+
+        if (loadTime > 2.5f)
+        {
+            SceneManager.LoadScene("Stage1");
+        }
     }
 }
